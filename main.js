13b6ba6fad7b604ed91cf782d20a4f6e7a4beaef
artemsites <info@artemsites.ru>
2023-09-20 12:28:07 +0400

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+// console.log( getDeclOfNum(44, ['минута', 'минуты', 'минут']) )
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+
+
+
+    let cartCount = document.querySelector('.cart-count');
+    let cartCountNum = cartCount.querySelector('.cart-count__num').textContent;
+    let cartCountWord = cartCount.querySelector('.cart-count__word');
+
+    cartCountWord.textContent = getDeclOfNum(cartCountNum, ['товар', 'товара', 'товаров'])
+
+
+
+  } catch (err) {
+    console.error(err)
+  } finally {}
+})
